036f3c2970b82198909d480ce5a067e0ee2575eb
Fr0ZZZT <newkosss@gmail.com>
2020-10-14 16:47:52 +0300

    test_items.py final code version

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_should_have_cart_button(browser):
     browser.get(link)
+    time.sleep(10)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert button, "The Cart Button is not present"
-    time.sleep(30)
+    assert button.is_displayed(), "The Cart Button is not present"
